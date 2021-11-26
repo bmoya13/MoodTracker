54661d7c27f2b20d4ee1cd8e940c3efc54a0c2cf
bmoya13 <bimoya@cpp.edu>
2021-11-26 13:12:49 -0800

    Initial Commit

---
+++ Shared/MoodTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodTrackerApp.swift
+//  Shared
+//
+//  Created by bmoya1301 on 11/26/21.
+//
+
+import SwiftUI
+
+@main
+struct MoodTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
